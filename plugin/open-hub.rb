1ba3e497d985c72997bc10fd7fc10acf645cb5ba
Kouhei Sutou <kou@clear-code.com>
2015-03-28 15:01:55 +0900

    Ohloh has been renamed to Open Hub

--- plugin/open-hub.rb
+++ plugin/open-hub.rb
@@ -1,6 +1,6 @@
 # -*- indent-tabs-mode: t; ruby-indent-level: 3; tab-width: 3 -*-
 #
-# Copyright (C) 2009  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2009-2015  Kouhei Sutou <kou@clear-code.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -15,10 +15,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-def ohloh_detail(id)
-	base = "http://www.ohloh.net/accounts/#{h(id)}"
+def open_hub_detail(id)
+	base = "https://www.openhub.net/accounts/#{u(id)}"
 	"<a href='#{base}?ref=Detailed'>" +
-		"<img alt='Ohloh profile for #{h(id)}' height='35' " +
-		"src='#{base}/widgets/account_detailed.gif' width='191' />" +
+		"<img alt='Open Hub profile for #{h(id)}' height='35' " +
+		"src='#{base}/widgets/account_detailed.gif' width='230'>" +
 		"</a>"
 end

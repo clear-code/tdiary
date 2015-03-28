ec2572009e12e365f59832339b00d6c7b4e670d0
Kouhei Sutou <kou@clear-code.com>
2015-03-28 15:50:26 +0900

    Reduce needless indent

--- html-archiver.rb
+++ html-archiver.rb
@@ -200,7 +200,7 @@ module HTMLArchiver
 			html5 = html5.sub(/<meta http-equiv="Content-Type" content="text\/html; charset=(.+?)">/) do
 				charset = $1
 				<<-HTML5
-		<meta charset="#{charset}">
+	<meta charset="#{charset}">
 				HTML5
 			end
 			html5

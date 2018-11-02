d442a2adefc6f847c0ba0a59526d8a10415f5fea
YUKI Hiroshi <shimoda@clear-code.com>
2018-11-02 17:06:20 +0900

    Add missing plugin

---
+++ plugin/similar-articles.rb
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def similar_articles
+	html = ''
+	if @similar_articles and not(@similar_articles.empty?)
+		html << "<div class=\"similar-articles\">\n"
+		html << "<h3>関連記事</h3>\n"
+		html << "<ul>\n"
+		@similar_articles.each do |article|
+			label = h(article["title"])
+			path = anchor(article["_key"])
+			html << "<li><a href=\"#{path}\">#{label}</a></li>\n"
+		end
+		html << "</ul>\n"
+		html << "</div>\n"
+	end
+	html
+end

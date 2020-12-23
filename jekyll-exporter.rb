19f6fee2b0239683b3771b43eb3ef9ee647238f9
Sutou Kouhei <kou@clear-code.com>
2020-12-23 17:23:17 +0900

    Add support for converting from Markdown

--- jekyll-exporter.rb
+++ jekyll-exporter.rb
@@ -24,28 +24,149 @@ require "optparse"
 require "pathname"
 require "yaml"
 
+require "commonmarker"
 require "rd/rdfmt"
 require "rd/rdvisitor"
 
-class RDParser
-  class PluginCall < BasicObject
-    attr_reader :name
-    attr_reader :args
-    def initialize(plugin_call)
-      instance_eval(plugin_call)
+module TitleParsable
+  def parse_title(title)
+    tags = []
+    while /\A\[(.+?)\]/ =~ title
+      tags << normalize_tag($1)
+      title = $POSTMATCH.strip
     end
+    [title, tags]
+  end
 
-    def method_missing(name, *args)
-      @name = name
-      @args = args
+  def normalize_tag(tag)
+    case tag
+    when "テスト"
+      "test"
+    when "会社"
+      "company"
+    when "インターンシップ"
+      "internship"
+    when "クリアなコード"
+      "clear-code"
+    when "フィードバック"
+      "feedback"
+    when "ノータブルコード"
+      "notable-code"
+    when "milter manager"
+      "milter-manager"
+    else
+      tag.downcase
+    end
+  end
+end
+
+class PluginCall < BasicObject
+  attr_reader :name
+  attr_reader :args
+  def initialize(plugin_call)
+    instance_eval(plugin_call)
+  end
+
+  def method_missing(name, *args)
+    @name = name
+    @args = args
+  end
+end
+
+class ImageFormatter
+  def initialize(context)
+    @context = context
+  end
+
+  def format(index, alt, thumbnail_index, size=nil)
+    image_url = resolve_image_url(index)
+    # TODO: Support size
+    if thumbnail_index
+      thumbnail_url = resolve_image_url(thumbnail_index)
+      "[" +
+        "![#{alt}]({{ \"#{thumbnail_url}\" | relative_url }} \"#{alt}\")" +
+        "]({{ \"#{image_url}\" | relative_url }})"
+    else
+      "![#{alt}]({{ \"#{image_url}\" | relative_url }} \"#{alt}\")"
     end
   end
 
+  private
+  def resolve_image_url(index)
+    image_path_glob = @context.date.strftime("%Y%m%d_#{index}.*")
+    image_path =
+      Pathname.glob(@context.images_output_dir + image_path_glob).first
+    "#{@context.images_path}/#{image_path.basename}"
+  end
+end
+
+class ISBNFormatter
+  def format(isbn, label=nil)
+    url = "https://amazon.co.jp/dp/#{isbn}"
+    "[#{label || url}](#{url})"
+  end
+end
+
+class YouTubeFormatter
+  def format(id)
+    <<-HTML
+<div class="youtube-4x3">
+  <iframe width="425"
+          height="350"
+          src="https://www.youtube.com/embed/#{id}"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen></iframe>
+</div>
+    HTML
+  end
+end
+
+module TextProcessable
+  def prepend(target, prefix)
+    case target
+    when Array
+      target.collect do |t|
+        prepend(t, prefix)
+      end
+    else
+      "#{prefix}#{target}"
+    end
+  end
+
+  def around(target, prefix, postfix)
+    case target
+    when Array
+      target.collect do |t|
+        around(t, prefix, postfix)
+      end
+    else
+      "#{prefix}#{target}#{postfix}"
+    end
+  end
+
+  def flatten(contents)
+    contents.collect do |content|
+      case content
+      when Array
+        content.flatten.join("\n")
+      else
+        content
+      end
+    end
+  end
+end
+
+class RDParser
   class Visitor < RD::Visitor
+    include TitleParsable
+    include TextProcessable
+
     def initialize(context)
       @context = context
+      @first_paragraph = true
       @title = nil
-      @categories = []
+      @tags = []
       @footnotes = []
       @foottexts = []
       super()
@@ -53,33 +174,21 @@ class RDParser
 
     def metadata
       {
-        "categories" => @categories,
+        "tags" => @tags,
         "title" => @title,
       }
     end
 
     def apply_to_DocumentElement(element, contents)
-      contents = contents.collect do |content|
-        case content
-        when Array
-          content.flatten.join("\n")
-        else
-          content
-        end
-      end
+      contents = flatten(contents)
       (contents + @foottexts).join("\n\n")
     end
 
     def apply_to_Headline(element, contents)
       marks = "#" * (element.level + 1)
-      headline = contents.join("")
-      while /\A\[(.+?)\]/ =~ headline
-        @categories << $1
-        headline = $POSTMATCH
-      end
-      headline = headline.strip
+      headline = contents.join("").strip
       if element.level == 1
-        @title = headline
+        @title, @tags = parse_title(headline)
         ""
       else
         "#{marks} #{headline}"
@@ -125,7 +234,12 @@ class RDParser
 
     def apply_to_TextBlock(element, contents)
       paragraph = contents.join("")
-      paragraph.strip
+      paragraph = paragraph.strip
+      if @first_paragraph
+        paragraph << "\n#{@context.excerpt_separator}\n"
+        @first_paragraph = false
+      end
+      paragraph
     end
 
     def apply_to_Verbatim(element)
@@ -150,13 +264,13 @@ class RDParser
       plugin_call = PluginCall.new(contents.join(""))
       case plugin_call.name
       when :image
-        format_image(*plugin_call.args)
+        ImageFormatter.new(@context).format(*plugin_call.args)
       when :isbn, :isbn_detail
-        format_isbn(*plugin_call.args)
+        ISBNFormatter.new.format(*plugin_call.args)
       when :bugzilla
         format_bugzilla(*plugin_call.args)
       when :youtube_div
-        format_youtube(*plugin_call.args)
+        YouTubeFormatter.new.format(*plugin_call.args)
       when :nicovideo
         format_nicovideo(*plugin_call.args)
       when :ustream
@@ -168,46 +282,8 @@ class RDParser
       end
     end
 
-    def resolve_image_url(index)
-      image_path_glob = @context.date.strftime("%Y%m%d_#{index}.*")
-      image_path =
-        Pathname.glob(@context.images_output_dir + image_path_glob).first
-      "#{@context.images_path}/#{image_path.basename}"
-    end
-
-    def format_image(index, alt, thumbnail_index, size=nil)
-      image_url = resolve_image_url(index)
-      # TODO: Support size
-      if thumbnail_index
-        thumbnail_url = resolve_image_url(thumbnail_index)
-        "[" +
-          "![#{alt}]({{ \"#{thumbnail_url}\" | relative_url }} \"#{alt}\")" +
-          "]({{ \"#{image_url}\" | relative_url }})"
-      else
-        "![#{alt}]({{ \"#{image_url}\" | relative_url }} \"#{alt}\")"
-      end
-    end
-
-    def format_isbn(isbn, label=nil)
-      url = "https://amazon.co.jp/dp/#{isbn}"
-      "[#{label || url}](#{url})"
-    end
-
     def format_bugzilla(id)
       "[#{id}](https://bugzilla.mozilla.org/show_bug.cgi?id=#{id})"
-    end
-
-    def format_youtube(id)
-      <<-HTML
-<div class="youtube-4x3">
-  <iframe width="425"
-          height="350"
-          src="https://www.youtube.com/embed/#{id}"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen></iframe>
-</div>
-      HTML
     end
 
     def format_nicovideo(id)
@@ -334,28 +410,6 @@ class RDParser
         language
       end
     end
-
-    def prepend(target, prefix)
-      case target
-      when Array
-        target.collect do |t|
-          prepend(t, prefix)
-        end
-      else
-        "#{prefix}#{target}"
-      end
-    end
-
-    def around(target, prefix, postfix)
-      case target
-      when Array
-        target.collect do |t|
-          around(t, prefix, postfix)
-        end
-      else
-        "#{prefix}#{target}#{postfix}"
-      end
-    end
   end
 
   def initialize(context, headers, body)
@@ -373,6 +427,224 @@ class RDParser
   end
 end
 
+class MarkdownParser
+  include TitleParsable
+  include TextProcessable
+
+  def initialize(context, headers, body)
+    @context = context
+    @headers = headers
+    @body = body
+    @tags = []
+    @title = nil
+    @first_paragraph = true
+    @indent = 0
+    @foottexts = []
+  end
+
+  def parse
+    prepared_body = process_plugin(@body)
+    doc = CommonMarker.render_doc(prepared_body)
+    markdown = visit(doc)
+    [
+      {
+        "tags" => @tags,
+        "title" => @title,
+      },
+      markdown,
+    ]
+  end
+
+  private
+  def process_plugin(text)
+    processed_text = ""
+    in_code_block = false
+    text.each_line do |line|
+      case line
+      when /\A(?:```|~~~)/
+        in_code_block = !in_code_block
+        processed_text << line
+      else
+        if in_code_block
+          processed_text << line
+        else
+          processed_text << line.gsub(/{{(.+?)}}/) do
+            plugin_call_text = $1
+            case plugin_call_text
+            when /\A#/, /\A\//, /\A\s*matrix/
+              "{% raw %}{{#{plugin_call_text}}}{% endraw %}"
+            else
+              plugin_call = PluginCall.new(plugin_call_text)
+              case plugin_call.name
+              when :fn
+                format_footnote(*plugin_call.args)
+              when :image
+                ImageFormatter.new(@context).format(*plugin_call.args)
+              when :isbn, :isbn_detail
+                ISBNFormatter.new.format(*plugin_call.args)
+              when :youtube_div
+                YouTubeFormatter.new.format(*plugin_call.args)
+              else
+                raise "TODO: #{__method__}: #{plugin_call.name} #{plugin_call.args.inspect}"
+              end
+            end
+          end
+        end
+      end
+    end
+    processed_text
+  end
+
+  def format_footnote(text)
+    index = @foottexts.size
+    @foottexts << "[^#{index}]: #{text}"
+    "[^#{index}]"
+  end
+
+  def visit(node)
+    contents = node.each.collect do |child|
+      visit(child)
+    end
+    __send__("visit_#{node.type}", node, contents)
+  end
+
+  def visit_text(node, contents)
+    node.string_content
+  end
+
+  def visit_header(node, contents)
+    if node.header_level == 1
+      @title, @tags = parse_title(contents.join("").strip)
+      ""
+    else
+      markup = "#" * (node.header_level + 1)
+      "#{markup} #{contents.join("")}"
+    end
+  end
+
+  def visit_link(node, contents)
+    url = node.url.force_encoding("UTF-8")
+    label = flatten(contents).join("")
+    case url
+    when /\A(\d{4})(\d{2})(\d{2})\z/
+      year = $1
+      month = $2
+      day = $3
+      "[#{label}]({% post_url #{year}-#{month}-#{day}-index %})"
+    else
+      "[#{label}](#{url})"
+    end
+  end
+
+  def visit_paragraph(node, contents)
+    paragraph = contents.join("")
+    if @first_paragraph
+      paragraph << "\n#{@context.excerpt_separator}\n"
+      @first_paragraph = false
+    end
+    paragraph
+  end
+
+  def visit_list_item(node, contents)
+    contents
+  end
+
+  def prepend_one_level(target, one_level_prefix, deeper_level_prefix)
+    case target
+    when Array
+      target.collect do |t|
+        case t
+        when Array
+          prepend(t, deeper_level_prefix)
+        else
+          # XXX: Ad-hoc fix
+          if t.start_with?("```")
+            t.gsub(/^/) {"#{deeper_level_prefix}"}
+          else
+            "#{one_level_prefix}#{t}"
+          end
+        end
+      end
+    else
+      "#{one_level_prefix}#{target}"
+    end
+  end
+
+  def visit_list(node, contents)
+    case node.list_type
+    when :bullet_list
+      contents.collect {|content| prepend_one_level(content, "  * ", "    ")}
+    when :ordered_list
+      contents.collect {|content| prepend_one_level(content, "  1. ", "   ")}
+    else
+      raise "unknown list type: #{node.list_type}"
+    end
+  end
+
+  def visit_softbreak(node, contents)
+    "\n"
+  end
+
+  def visit_linebreak(node, contents)
+    "\n\n"
+  end
+
+  def visit_code_block(node, contents)
+    markdown = <<-MARKDOWN
+```#{node.fence_info.force_encoding("UTF-8")}
+#{node.string_content}```
+    MARKDOWN
+    if node.string_content.include?("{{")
+      markdown = "{% raw %}\n#{markdown}{% endraw %}\n"
+    end
+    markdown
+  end
+
+  def visit_code(node, contents)
+    "`#{node.string_content}`"
+  end
+
+  def visit_blockquote(node, contents)
+    contents.inject("") do |result, content|
+      result << "> #{content}\n"
+      result
+    end
+  end
+
+  def visit_emph(node, contents)
+    "*#{contents.join("")}*"
+  end
+
+  def visit_html(node, contents)
+    node.string_content
+  end
+
+  def visit_inline_html(node, contents)
+    node.string_content
+  end
+
+  def visit_hrule(node, contents)
+    "---"
+  end
+
+  def visit_strong(node, contents)
+    "**#{contents.join("")}**"
+  end
+
+  def visit_image(node, contents)
+    if contents.empty?
+      return "![#{node.title}](#{node.url})"
+    end
+    pp node
+    pp contents
+    raise
+  end
+
+  def visit_document(node, contents)
+    (contents + @foottexts).flatten.join("\n\n")
+  end
+end
+
 class JekyllExporter
   class Context
     attr_accessor :date
@@ -380,12 +652,14 @@ class JekyllExporter
     attr_accessor :posts_output_dir
     attr_accessor :images_output_dir
     attr_accessor :images_path
+    attr_accessor :excerpt_separator
     def initialize
       @date = nil
       @data_dir = "data"
       @posts_output_dir = "_posts"
       @images_output_dir = "images/blog"
       @images_path = "/images/blog"
+      @excerpt_separator = "<!--more-->"
     end
   end
 
@@ -423,6 +697,11 @@ class JekyllExporter
               "(#{@context.images_path})") do |path|
       @context.images_path = path
     end
+    parser.on("--excerpt-separator=SEPARATOR",
+              "The except separator",
+              "(#{@context.excerpt_separator})") do |separator|
+      @context.excerpt_separator = separator
+    end
     parser.parse!(args)
   end
 
@@ -439,6 +718,7 @@ class JekyllExporter
 
   def export_posts
     td2_paths = Dir.glob(File.join(@context.data_dir, "????", "*.td2"))
+    # td2_paths = Dir.glob(File.join(@context.data_dir, "2020", "202009.td2"))
     td2_paths.sort.each do |td2_path|
       File.open(td2_path) do |td2_file|
         parse_td2(td2_file) do |headers, body|
@@ -532,7 +812,8 @@ class JekyllExporter
   end
 
   def parse_diary_markdown(headers, body)
-    [{}, ""]
+    parser = MarkdownParser.new(@context, headers, body)
+    parser.parse
   end
 end
 
